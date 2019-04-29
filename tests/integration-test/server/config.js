85f1685b76aa7fc471c14fc3b8312107f2ce266c
hoddr <hoddr@outlook.com>
2019-04-29 14:47:32 -0400

    No inputs defined in config for now

--- tests/integration-test/server/config.js
+++ tests/integration-test/server/config.js
@@ -5,7 +5,6 @@ const { join } = require('path');
 const PORT = 4000;
 
 module.exports = {
-  input: '', // TODO fill in input file path here?
   STORAGE_BASE_DIR: join(__dirname, './storage'),
   API_TOKEN: 'my-very-special-key12345',
   API_BASE_URL: `http://localhost:${PORT}/v1`,
