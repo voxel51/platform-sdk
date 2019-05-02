47681d9f23adf7c14d9d9b6dcd57724914cf259a
hoddr <hoddr@outlook.com>
2019-05-02 09:21:21 -0400

    Fixes cleanup script

--- tests/integration-test/server/cleanup.js
+++ tests/integration-test/server/cleanup.js
@@ -6,7 +6,7 @@ const config = require('./config.js');
 
 (function main() {
   console.log('----- Cleaning up temporary storage files ------ ');
-  exec(`rm * ${config.STORAGE_BASE_DIR}`, (err, stdout, stderr) {
+  exec(`rm ${config.STORAGE_BASE_DIR}/*`, (err, stdout, stderr) => {
     if (err) console.error(err);
     console.log('----- Cleanup complete -----');
   });
