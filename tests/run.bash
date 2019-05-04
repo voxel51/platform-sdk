a0a27db5f5d2ae21ef320627757a1f1d7fd93a03
brimoor <brimoor@umich.edu>
2019-05-04 15:19:38 -0400

    running the server directly

--- tests/run.bash
+++ tests/run.bash
@@ -1,18 +1,13 @@
 #!/bin/bash
-# Local test suite runner script.
+# Local test server runner script.
 #
-# Usage:
-# bash run.bash \
-#     --analytic-image='image-name' \
-#     --analytic-json='/path/to/analytic.json' \
-#     [ --inputs='<input1>:<path1>,...' ] \
-#     [ --params='{"<param1>": <value1>, ...}' ] \
-#     [ --compute-type=cpu|gpu ]
+# To see usage:
+#   bash run.bash --help
 #
 # Copyright 2017-2019, Voxel51, Inc.
 # voxel51.com
 #
-# David Hodgson, david@voxel51.com
+# Brian Moore, brian@voxel51.com
 #
 
-npm run start -- "$@"
+node server/index.js "$@"
