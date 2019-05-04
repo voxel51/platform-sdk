cc29cd74811227b58930cd4c6bd37ab1b370ee6e
brimoor <brimoor@umich.edu>
2019-05-04 01:31:49 -0400

    adding script to run cleanup

---
+++ tests/clean.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Cleans up generated test files.
+#
+# Usage:
+#   bash clean.bash
+#
+# Copyright 2017-2019, Voxel51, Inc.
+# voxel51.com
+#
+# David Hodgson, david@voxel51.com
+#
+
+npm run clean
