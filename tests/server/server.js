22858607dab857cba733463151b983b5b61b5745
brimoor <brimoor@umich.edu>
2019-05-04 21:33:04 -0400

    tweaking docstring

--- tests/server/server.js
+++ tests/server/server.js
@@ -1,10 +1,11 @@
 /**
- * Main local testing server.
+ * Local testing server.
  *
  * Copyright 2017-2019, Voxel51, Inc.
  * voxel51.com
  *
  * David Hodgson, david@voxel51.com
+ * Brian Moore, brian@voxel51.com
  */
 'use strict';
 
