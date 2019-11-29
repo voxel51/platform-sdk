181172f877aa5930129530b12d0d9e522f569457
brimoor <brimoor@umich.edu>
2019-11-29 12:13:44 -0500

    adding test server to install script

--- install.bash
+++ install.bash
@@ -40,3 +40,20 @@ echo "Installing ETA lite"
 cd eta
 bash install.bash -l
 cp config-example.json config.json
+
+# Install local test server
+echo "Installing local test server"
+command -v npm &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "Installing Node.js"
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+    if [[ -f ~/.bashrc ]]; then
+        source ~/.bashrc
+    fi
+    if [[ -f ~/.bash_profile ]]; then
+        source ~/.bash_profile
+    fi
+    nvm install node
+fi
+cd tests/platform
+npm install
