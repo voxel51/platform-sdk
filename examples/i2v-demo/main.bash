25922747da74157b166e2fcc2ddbe3a13917051f
brimoor <brimoor@umich.edu>
2019-12-04 10:20:38 -0500

    fixing log appending bug in main.bash scripts

--- examples/i2v-demo/main.bash
+++ examples/i2v-demo/main.bash
@@ -1,23 +1,31 @@
 #!/bin/bash
 # Main entrypoint for an Image-to-Video container.
 #
-# Syntax:
-#   bash main.bash
+# Copyright 2017-2019, Voxel51, Inc.
+# voxel51.com
 #
 
 #
-# Don't change this path; the platform attaches a pre-stop hook to images at
-# runtime that will upload the logfile from this location whenever a task is
+# Don't change `LOGFILE_PATH`. The platform attaches a pre-stop hook to images
+# at runtime that will upload the logfile from this location whenever a task is
 # terminated unexpectedly (e.g., preemption, resource violation, etc.)
 #
 LOGFILE_PATH=/var/log/image.log
+BACKUP_LOGFILE_PATH=/var/log/backup.log
 
 #
 # Execute analytic and pipe stdout/stderr to disk so that this information
 # will be available in case of errors.
 #
-# If necessary, replace `python main.py` here with the appropriate invocation
-# for your analytic.
-#
 set -o pipefail
-python main.py 2>&1 | tee -a "${LOGFILE_PATH}"
+python main.py 2>&1 | tee "${BACKUP_LOGFILE_PATH}"
+
+# Gracefully handle uncaught failures in analytic
+if [ $? -ne 0 ]; then
+    #
+    # An uncatught exception occurred when executing the analytic, so append
+    # the backup log to the logfile, just in case
+    #
+    echo "UNCAUGHT EXCEPTION; APPENDING BACKUP LOG" >> "${LOGFILE_PATH}"
+    cat "${BACKUP_LOGFILE_PATH}" >> "${LOGFILE_PATH}"
+fi
